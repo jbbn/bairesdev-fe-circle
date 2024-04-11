2aeaac1f961da45b8726ef90c0a2ea63616cd0ff
Bueno (João) <3846247+jbbn@users.noreply.github.com>
2024-04-11 00:10:53 -0300

    ✨(user-scripts): Employee Portal Login: Custom Image
    Add user script for customizing the image that shows on the BairesDev's Employee Portal.

---
+++ user-scripts/employee-portal-login-custom-image.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         BD - Employee Portal - Login: Custom Image
+// @namespace    https://github.com/jbbn/bairesdev-fe-circle/tree/master/user-scripts
+// @version      2024-04-10
+// @description  Overwrite the Image of the BairesDev's Employee Portal sign in page with a custom image
+// @author       https://github.com/jbbn
+// @match        https://employees.bairesdev.com/login
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bairesdev.com
+// @require      https://raw.githubusercontent.com/jbbn/bairesdev-fe-circle/master/user-scripts/util/waitForElem.js
+// @grant        none
+// ==/UserScript==
+
+const profileImageUrl = localStorage.getItem("BD_EP_PROFILE_IMAGE_URL");
+
+if (profileImageUrl) {
+  const imageSelector = 'body > bdv-root > bdv-login > div > div.right-content > div > div > img'
+
+  async function custom() {
+    const imageElement = await UserScript.waitForElem(imageSelector)
+    imageElement.src = profileImageUrl
+    imageElement.style.width = 'auto'
+  }
+
+  window.addEventListener('load', custom, false);
+}
